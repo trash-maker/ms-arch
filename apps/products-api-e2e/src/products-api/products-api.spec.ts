3ab65d9455a6415bcaf9483b1312c377f9217730
trash-maker <undesired.mails.here@gmail.com>
2024-01-21 16:26:13 +0100

    feat(products): :sparkles: add endpoints for managing data
    json-server based endpoints through router

--- apps/products-api-e2e/src/products-api/products-api.spec.ts
+++ apps/products-api-e2e/src/products-api/products-api.spec.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/`);
+describe('GET /health', () => {
+  it('should return UP status', async () => {
+    const res = await axios.get(`/health`);
 
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+    expect(res.data).toEqual({ status: 'UP' });
   });
 });
